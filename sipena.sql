7e8624f73afd82f197b87cd9135e7d512a260b6a
ricomarten <ricomarten@gmail.com>
2018-07-03 11:47:15 +0700

    update sql

--- sipena.sql
+++ sipena.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2018 at 02:08 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 5.6.34
+-- Generation Time: Jul 03, 2018 at 06:45 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,82 @@ SET time_zone = "+00:00";
 --
 -- Database: `sipena`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kegiatan`
+--
+
+CREATE TABLE `kegiatan` (
+  `id` int(11) NOT NULL,
+  `nama` text NOT NULL,
+  `jadwal` text NOT NULL,
+  `target_dokumen` int(11) NOT NULL,
+  `waktu_olah` int(11) NOT NULL,
+  `jam_kerja` int(11) NOT NULL,
+  `hari_kerja` int(11) NOT NULL,
+  `pc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaturan`
+--
+
+CREATE TABLE `pengaturan` (
+  `pc` int(11) NOT NULL,
+  `jam_kerja` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengaturan`
+--
+
+INSERT INTO `pengaturan` (`pc`, `jam_kerja`) VALUES
+(6, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaturan2`
+--
+
+CREATE TABLE `pengaturan2` (
+  `tanggal` date NOT NULL,
+  `jam_kerja` int(11) NOT NULL,
+  `pc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengaturan2`
+--
+
+INSERT INTO `pengaturan2` (`tanggal`, `jam_kerja`, `pc`) VALUES
+('2018-07-07', 10, 6),
+('2018-07-18', 10, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `risiko`
+--
+
+CREATE TABLE `risiko` (
+  `id` int(11) NOT NULL,
+  `risiko` text NOT NULL,
+  `mitigasi` text NOT NULL,
+  `status` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `risiko`
+--
+
+INSERT INTO `risiko` (`id`, `risiko`, `mitigasi`, `status`) VALUES
+(1, 'Listrik sering padam', 'Menggunakan genset kantor', 'Tindakan mitigasi bagus'),
+(2, 'Tenaga pengolahan mengundurkan diri', 'Membuat daftar cadangan petugas yang sewaktu-waktu dapat dipanggil', 'Perlu ada tindakan mitigasi yang lebih baik');
 
 -- --------------------------------------------------------
 
@@ -60,11 +136,32 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `password`, `status`) VALUES
-('ricomarten', '123456', '0');
+('admin', '123456', '0'),
+('intan', '123456', '3'),
+('joshua', '123456', '1'),
+('ricomarten', '123456', '2');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengaturan2`
+--
+ALTER TABLE `pengaturan2`
+  ADD PRIMARY KEY (`tanggal`);
+
+--
+-- Indexes for table `risiko`
+--
+ALTER TABLE `risiko`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `status_user`
@@ -77,6 +174,22 @@ ALTER TABLE `status_user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `risiko`
+--
+ALTER TABLE `risiko`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
